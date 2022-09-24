6da5694a9a51aa87e19513f676c2ab26a75e8af7
Hui-zy <114277135+Hui-zy@users.noreply.github.com>
2022-09-24 15:11:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wxe0590206c6c4c8a3",
+  APP_ID: "",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "6fb3183700593391309d72415f744120",
+  APP_SECRET: "",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -57,12 +57,12 @@ export const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [o65R75lxBC9pEuYiTMCXOA4Ke7R8，o65R75o8fUnSpRTQgSXmp67EE8bY]
+  USERS: []
     {
       // 想要发送的人的名字
       name: "宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "祝宇宝宝，早上好吖~",
+      id: "宝宝，早上好吖~",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "",
       // 所在省份
@@ -90,7 +90,7 @@ export const USER_CONFIG = {
      
       name: "宝宝",
       id: "祝宇宝宝，早上好吖~",
-      useTemplateId: "C3Hxo2WWw-uDpAcXv_Vu7sUSNBMRJIzooi1-6moQUsI",
+      useTemplateId: "",
       province: "上海",
       city: "上海",
       horoscopeDate: '08-01',
